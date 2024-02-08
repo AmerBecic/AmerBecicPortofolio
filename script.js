6f85519ebbb697d4f6b74c1a1eaf98c6288415f5
Amer Becic <amer.becic999@gmail.com>
2024-02-08 19:37:57 +0100

    Added About me description and made all links open in new tab

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+function OpenLinkedIn() {
+    window.open("https://www.linkedin.com/in/amer-bečić-330076242", "_blank");
+}
+
+function OpenGitHub() {
+    window.open("https://github.com/AmerBecic", "_blank");
+}
